2cad88f19275918fe96187595c8a2b234fb26f6d
Joaquin <buzetajoaquin@gmail.com>
2022-09-25 15:55:16 -0300

    Inicial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import MainSection from "./Components/MainSection"
+
+const App = () => {
+  return (
+    <body className="bg-black h-screen">
+      <header className="flex justify-center">
+        <h1 className="text-white text-5xl">Musical Instruments</h1>
+      </header>
+      <MainSection/>
+    </body>
+  )
+}
+
+export default App
---
+++ src/Components/MainSection.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const MainSection = () => {
+    return (
+            <section className="flex">
+                    <div className="w-1/4">
+                        <img className="h-full" src="https://img.freepik.com/premium-photo/brass-instruments-lie-chairs-during-break-symphonic-music-concert_327282-204.jpg?w=2000" alt="img brass"></img>
+                    </div>
+                    <div className="w-1/4">
+                        <img className="h-full" src="https://cdn.pixabay.com/photo/2021/01/20/22/06/drums-5935753_1280.jpg" alt="img drums"></img>
+                    </div>
+                    <div className="w-1/4">
+                        <img className="h-full" src="https://i0.wp.com/www.iedunote.com/img/35406/instruments-in-the-woodwind-family-what-you-need-to-know.jpg" alt="img woodwind"></img>
+                    </div>
+                    <div className="w-1/4">
+                        <img className="h-full" src="https://c1.wallpaperflare.com/preview/803/676/975/cello-music-instrument-classic.jpg" alt="img strings"></img>
+                    </div>
+                    <div className="w-1/4">
+                        <img className="h-full" src="https://c0.wallpaperflare.com/preview/218/233/171/piano-oldschool-vintage-music.jpg" alt="img piano"></img>
+                    </div>
+            </section>
+    )
+}
+
+export default MainSection
